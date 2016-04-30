664c97f56add2202951e06d6094953e6733958e5
shiran <shiran.mazor@gmail.com>
2016-04-30 12:59:34 +0300

    add new files and folders

---
+++ TwitterApi/load_data.py
@@ -0,0 +1,12 @@
+'''
+this module will contain functions for filling and updaing the db from twiter
+uses twitter_api module
+1. insert_all_data
+2. update_existing_data(from_date)
+3. insert_user_table
+4. insert_new_tweets(user_id)
+'''
+import sys
+
+from twitter_api import *
+from DB.db_wrapper import *
---
+++ TwitterApi/twitter_api.py
@@ -0,0 +1,8 @@
+'''
+this module will contain abstract functions for twiter api
+author: avi
+functions example : get_tweets(user_id,from_date, recort_count), get_followers(user_id,count)
+
+'''
+
+from twitter import *
