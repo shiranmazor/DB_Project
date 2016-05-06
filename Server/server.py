2af3930f69aa9b43ef28f95182d000c3b9b8a08a
mattans <ms0fisrael>
2016-05-06 23:18:45 +0300

    added web functionality

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, url_for
+import sys
+sys.path.append("../")
+from ServerLogic.common import *
 
 application = Flask(__name__)
 
@@ -6,9 +9,22 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     data = {"params": ["sharon", "yoyo", "fuck off!", "bla bla bla..."],
-            "title": "Hello Mates"}
+            "title": "Hello Mates", "users_tuples": users_data_sorted}
     return render_template('index.html', **data)
 
+@application.route("/bottom", methods = ['GET', 'POST'])
+def bottom():
+    screen_name_1 = str(request.form['screen_name_1'])
+    screen_name_2 = str(request.form['screen_name_2'])
+    html_pattern = "<br />{0} and {1} are both ".format(users_data[screen_name_1]["real_name"], users_data[screen_name_2]["real_name"])
+    html = ""
+    if users_data[screen_name_1]["role"] == users_data[screen_name_2]["role"]:
+        html += (html_pattern + users_data[screen_name_1]["role"] + "s")
+    if users_data[screen_name_1]["state"] == users_data[screen_name_2]["state"]:
+        html += (html_pattern + "from " + users_data[screen_name_1]["state"])
+    if len(users_data[screen_name_1]["party"]) == 1 and users_data[screen_name_1]["party"] == users_data[screen_name_2]["party"]:
+        html += (html_pattern + users_data[screen_name_1]["party"].replace("R", "Republicans").replace("D", "Democrats"))
+    return html
 
 @application.route("/test")
 def test():
--- ServerLogic/common.py
+++ ServerLogic/common.py
@@ -1,9 +1,13 @@
 import csv
 from consts import *
+import sys
+sys.path.append("../")
 
 name_to_screen = {}
 screen_to_name = {}
 users_data = {}
+
+
 
 def load_users_from_csv():
     try:
@@ -20,7 +24,16 @@ def load_users_from_csv():
         print ex.message
 
 def load_users_dict():
-    pass
+    try:
+        with open(CSV_PATH, 'rb') as csvfile:
+            csv_data = csv.DictReader(csvfile)
+            for row in csv_data:
+                users_data[row["screen_name"]] = row
+    except Exception as ex:
+        print 'Error in parsing CSV file! dictionaries will be Empty!'
+        print ex.message
 
 
 load_users_from_csv()
+load_users_dict()
+users_data_sorted = sorted(name_to_screen.items(), key=lambda item: item[0])
