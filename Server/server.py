ae36dab9500c4c4894e600ba29db7ee1455df81a
Avi <avi.c33@gmail.com>
2016-05-27 14:48:38 +0300

    Fixed and added few stuff to server to work with DB 2

--- Server/server.py
+++ Server/server.py
@@ -13,21 +13,20 @@ def hello():
 
     def create_tuples(*fields):
         try:
-            keys = [x['screen_name'] for x in users_data]
             result = []
-            for user in users_data:
+            for user in users_data.keys():
                 # print user
                 # print str(users_data)
-                temp = [user["screen_name"]]
+                temp = [users_data[user]["screen_name"]]
                 for field in fields:
-                    temp.append(user[field])
+                    temp.append(users_data[user][field])
                 result.append(temp)
                 # print str(temp)
             return sorted(result, key=lambda item: item[1])
         except:
             print traceback.format_exc()
 
-    print str(create_tuples("full_name", "party_name"))
+    #print str(create_tuples("full_name", "party_name"))
     data = {"params": ["sharon", "yoyo", "fuck off!", "bla bla bla..."],
             "title": "Hello Mates", "users_tuples": create_tuples("full_name", "party_name"),
             "select_names": ["screen_name_1", "screen_name_2"]}
@@ -43,22 +42,23 @@ def bottom():
     screen_name_2 = str(request.form['screen_name_2'])
 
     def compare_fields(field):
-        return (users_data[screen_name_1][field] == users_data[screen_name_2][field])
+        return users_data[screen_name_1][field] == users_data[screen_name_2][field]
 
     html = ""
-    if compare_fields("full_name"):
+    if screen_name_1 == screen_name_2:
         html = "<br />You chose to compare the same person!"
-        return html
     else:
         html_pattern = "<br />{0} and {1} are both ".format(users_data[screen_name_1]["full_name"],
                                                             users_data[screen_name_2]["full_name"])
-
-    if compare_fields("role"):
-        html += (html_pattern + users_data[screen_name_1]["role"] + "s")
-    if compare_fields("state"):
-        html += (html_pattern + "from " + users_data[screen_name_1]["state"])
-    if users_data[screen_name_1]["party_name"] in ("D", "R") and compare_fields("party_name"):
-        html += (html_pattern + str_replace(users_data[screen_name_1]["party_name"],"R", "Republicans","D", "Democrats"))
+        try:
+            if compare_fields("role_name"):
+                html += (html_pattern + users_data[screen_name_1]["role_name"] + "s")
+            if compare_fields("state"):
+                html += (html_pattern + "from " + users_data[screen_name_1]["state"])
+            if users_data[screen_name_1]["party_name"] in ("Democratic", "Republican") and compare_fields("party_name"):
+                html += (html_pattern + str_replace(users_data[screen_name_1]["party_name"], "Republican", "Republicans", "Democratic", "Democrats"))
+        except:
+            print traceback.format_exc()
     return html
 
 @application.route("/test")
