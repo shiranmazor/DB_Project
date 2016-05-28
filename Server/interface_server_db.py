494e9fe19165e399296934fb48d153872b2efa08
Avi <avi.c33@gmail.com>
2016-05-28 13:41:37 +0300

    Added getting shared tweets and mentions

--- Server/interface_server_db.py
+++ Server/interface_server_db.py
@@ -1,5 +1,7 @@
 import ServerLogic.user_data as ud
 from ServerLogic.friendship_data import *
+from ServerLogic.searches_logic import *
+from time import gmtime, strftime
 import traceback
 
 users_data = ud.get_user_list()
@@ -16,11 +18,7 @@ def create_tuples(*fields):
     except:
         print traceback.format_exc()
 
-def compare_fields(field, screen_name_1, screen_name_2):
-    return users_data[screen_name_1][field] == users_data[screen_name_2][field]
-
 def get_friendship(screen_name_1, screen_name_2):
-    html = ""
     if screen_name_1 == screen_name_2:
         html = "<br />You chose to compare the same person! Please choose different persons"
     else:
@@ -38,23 +36,55 @@ def get_friendship(screen_name_1, screen_name_2):
                 if key == "location":
                     html += (html_pattern + "from " + shared_info["location"])
                 if key == "followers":
-                    followers = str(shared_info["followers"]).strip('[]')
-                    if followers != "":
+                    followers = shared_info["followers"]
+                    followers = followers[0:100]
+                    followers = str(followers).strip('[]')
+                if followers != "":
                         html += (html_pattern + "have the common followers: " + followers)
                 if key == "followees":
-                    followees = str(shared_info["followees"]).strip('[]')
+                    followees = shared_info["followees"]
+                    followees = followees[0:100]
+                    followees = str(followees).strip('[]')
                     if followees != "":
                         html += (html_pattern + "and follow: " + followers)
         except:
             print traceback.format_exc()
     return html
+
+def get_shared_tweets(screen_name_1, screen_name_2):
+    shared = get_shared_tweets(screen_name_1, screen_name_2)
+    shared_tweets = shared[0]
+    shared_tweets = shared_tweets[0:20]
+    return "<br /> The shared Tweets are: ".format(str(shared_tweets).strip('[]'))
+
+def get_tweets_user_mentions(screen_name_1, screen_name_2):
+    shared = get_shared_tweets(screen_name_1, screen_name_2)
+
+    user1_mentions = shared[1]
+    user1_mentions = user1_mentions[0:10]
+    user1_mentions = str(user1_mentions).strip('[]')
+
+    user2_mentions = shared[2]
+    user2_mentions = user2_mentions[0:10]
+    user2_mentions = str(user2_mentions).strip('[]')
+
+    return "<br /> {0} was mentioned in: {1}. {2} was mentioned in: {3}".format(users_data[screen_name_2]["full_name"], user1_mentions, users_data[screen_name_1]["full_name"], user2_mentions)
+
+
 def get_user_data(screen_name):
-    html = "<br /> <img src={0}>".format(users_data[screen_name]["profile_picture_url"])
-    html += "<br />{0} ".format(users_data[screen_name]["full_name"])
-    html += "<br />is a {0} ".format(users_data[screen_name]["role_name"])
-    html += "<br />from {0} ".format(users_data[screen_name]["location"])
+    html = "<br /> <img src={}>".format(users_data[screen_name]["profile_picture_url"])
+    html += "<br />{} ".format(users_data[screen_name]["full_name"])
+    html += "<br />is a {} ".format(users_data[screen_name]["role_name"])
+    html += "<br />from {} ".format(users_data[screen_name]["location"])
     html += ""
     return html
 
-def get_shared(screen_name_1):
-    pass
+def get_popular_searched():
+    searched_list = str(get_popular_users(3).strip('[]'))
+    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    return "<br /> The most popular congress members searched, till {0} are: {1}".format(date, searched_list)
+
+def update_search(screen_name):
+    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    update_user_search(screen_name, date)
+    return
