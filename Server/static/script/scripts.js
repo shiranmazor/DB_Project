486436fe55a7e5974558cf87a599cfa0ac830d1d
Avi <avi.c33@gmail.com>
2016-05-31 22:30:53 +0300

    Made distribution of all static files.

---
+++ Server/static/script/scripts.js
@@ -0,0 +1,42 @@
+// assumption: dropdown names are "screen_name_1", "screen_name_2"
+
+function validateSelection() {
+    if (document.forms.selection.screen_name_1.value == document.forms.selection.screen_name_2.value) {
+        alert('Warning: chose same person twice');
+        document.forms.selection.submit.disabled = true;
+    }
+    else {
+        document.forms.selection.submit.disabled = false;
+    }
+}
+
+function post(path, target, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("target", target);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+function refresh_bottom() {
+    document.getElementById("bottom").src = document.getElementById("bottom").src;
+}
+function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
+}
