3f8047854c92bb7b42e005bb262e40fdfb2cf602
shiran <shiran.mazor@gmail.com>
2016-06-06 00:14:37 +0300

    utilities

--- Utillities/usefull_queries.sql
+++ Utillities/usefull_queries.sql
@@ -10,3 +10,52 @@ CREATE INDEX full_name_index ON  users(full_name) USING BTREE;
 
 
 ALTER DATABASE hoc_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--complex sql query:
+--1. getting the followee_id of the user wuth maximun followers in our system:
+select follow.followee_id, max(followers_num) as followers_count
+from
+(
+select followee_id, count(follower_id) as followers_num
+from followers
+group by followee_id
+) as follow
+
+--2. num of searches
+select s.count
+from searches as s
+where s.user_id in
+(
+select follow2.followee_id
+from
+	(
+	select follow.followee_id, max(followers_num) as followers_count
+	from
+		(
+		select followee_id, count(follower_id) as followers_num
+		from followers
+		group by followee_id
+		) as follow
+	) as follow2
+)
+
+--2. getting the user_id, full_name, screen_name, search count, followers count of the populaar user
+
+select u.id, u.screen_name, u.full_name, ss.user_id
+
+select s.user_id s.count , follow2.followers_count
+from searches as s
+where s.user_id in
+(
+select follow2.followee_id , follow2.followers_count
+from
+	(
+	select follow.followee_id, max(followers_num) as followers_count
+	from
+		(
+		select followee_id, count(follower_id) as followers_num
+		from followers
+		group by followee_id
+		) as follow
+	) as follow2
+) as ss
--- wsgi/flask.wsgi
+++ wsgi/flask.wsgi
@@ -4,7 +4,7 @@ import sys
 sys.path.append("../")
 
 ##Virtualenv Settings
-activate_this = '/home/student/desktop/hoc/DB_Project/venv/bin/activate_this.py'
+activate_this = '/specific/a/home/cc/students/cs/shiranmazor/HOC/DB_Project/venv/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
 ##Replace the standard out
@@ -14,7 +14,7 @@ sys.stdout = sys.stderr
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..'))
 
 ##Add this file path to sys.path in order to import app
-sys.path.append('/home/student/desktop/hoc/DB_Project')
+sys.path.append('/specific/a/home/cc/students/cs/shiranmazor/HOC/DB_Project')
 
 ##Create appilcation for our app
 from Server.server import application as application
